d42ec9ef5317027c64f57ab684b16d6da8592149
Saurabh Gupta <saurabh58833@gmail.com>
2025-06-09 09:39:04 +0530

    Create pdf-merger.py

---
+++ pdf-merger.py
@@ -0,0 +1,9 @@
+# Combines multiple PDFs using PyPDF2.
+
+from PyPDF2 import PdfMerger
+
+merger = PdfMerger()
+merger.append("file1.pdf")
+merger.append("file2.pdf")
+merger.write("merged.pdf")
+merger.close()
